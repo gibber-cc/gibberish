f2f0c7b63cb3f3f2b06ca7359043df481ece4a40
charlieroberts <charlie@charlie-roberts.com>
2016-09-24 16:23:16 -0400

    fix for kps gain

--- js/karplusstrong.js
+++ js/karplusstrong.js
@@ -11,16 +11,18 @@ module.exports = function( Gibberish ) {
         frequency = g.in('frequency'),
         delay = g.delay( g.add( impulse, feedback.out ), div( Gibberish.ctx.sampleRate, frequency ), { size:2048 }),
         decayed = g.mul( delay, g.t60( g.mul( g.in('decay'), frequency ) ) ),
-        damped = g.mul( g.mix( decayed, feedback.out, g.in('damping') ), g.in('gain') )
+        damped =  g.mix( decayed, feedback.out, g.in('damping') ),
+        withGain = g.mul( damped, g.in('gain') )
 
     feedback.in( damped )
 
     props = Object.assign( {}, KPS.defaults, props )
 
-    let panner = g.pan( damped, damped, g.in( 'pan' ) ),
+    let panner = g.pan( withGain, withGain, g.in( 'pan' ) ),
         syn = Gibberish.factory( [panner.left, panner.right], 'synth', props  )
 
     syn.env = trigger
+
     syn.note = freq => {
       syn.frequency = freq
       syn.env.trigger()
