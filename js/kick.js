4c81ac1ba5988a1b596eb8f5181eaae260ab6bae
charlieroberts <charlie@charlie-roberts.com>
2016-09-27 14:02:58 -0400

    start of kick

---
+++ js/kick.js
@@ -0,0 +1,65 @@
+let g = require( 'genish.js' )
+
+module.exports = function( Gibberish ) {
+
+  let Synth = props => {
+    let osc, 
+        env = g.ad( g.in('attack'), g.in('decay'), { shape:'linear' }),
+        frequency = g.in( 'frequency' ),
+        phase
+
+    props = Object.assign( {}, Synth.defaults, props )
+
+    switch( props.waveform ) {
+      case 'saw':
+        osc = g.phasor( frequency )
+        break;
+      case 'square':
+        phase = g.phasor( frequency, 0, { min:0 } )
+        osc = lt( phase, .5 )
+        break;
+      case 'sine':
+        osc = cycle( frequency )
+        break;
+      case 'pwm':
+        phase = g.phasor( frequency, 0, { min:0 } )
+        osc = lt( phase, g.in( 'pulsewidth' ) )
+        break;
+    }
+
+    let oscWithGain = g.mul( g.mul( osc, env ), g.in( 'gain' ) ),
+        panner = g.pan( oscWithGain, oscWithGain, g.in( 'pan' ) ),
+        //syn = Gibberish.factory( oscWithGain, 'synth', props  )
+        syn = Gibberish.factory( [panner.left, panner.right], 'synth', props  )
+    
+    syn.env = env
+
+    syn.note = freq => {
+      syn.frequency = freq
+      syn.env.trigger()
+    }
+
+    syn.trigger = syn.env.trigger
+
+    syn.free = () => {
+      Gibberish.genish.gen.free( [panner.left, panner.right] )
+    }
+
+    //delete syn.toString()
+
+    return syn
+  }
+  
+  Synth.defaults = {
+    waveform:'saw',
+    attack: 44100,
+    decay: 44100,
+    gain: 1,
+    pulsewidth:.25,
+    frequency:220,
+    pan: .5
+  }
+
+  return Synth
+
+}
