c508c78a100a6a840b0dfc666542d31b208f472a
charlieroberts <charlie@charlie-roberts.com>
2016-10-24 22:48:24 -0400

    sampler supports loop points and non-looping behavior

--- js/instruments/sampler.js
+++ js/instruments/sampler.js
@@ -2,44 +2,65 @@ let g = require( 'genish.js' ),
     instrument = require( './instrument.js' )
 
 module.exports = function( Gibberish ) {
+  let proto = Object.create( instrument )
+
+  Object.assign( proto, {
+    note( rate ) {
+      this.rate = rate
+      if( rate > 0 ) {
+        this.trigger()
+      }else{
+        this.__accum__.value = this.data.buffer.length - 1 
+      }
+    }
+  })
 
   let Sampler = inputProps => {
-    let syn = Object.create( instrument )
+    let syn = Object.create( proto )
 
     let props = Object.assign( {}, Sampler.defaults, inputProps )
     
-    let env = g.ad( g.in('attack'), g.in('decay'), { shape:'linear' }),
-        rate = g.in( 'rate' ),
-        initialized  = g.param( 'initailied', 0 ),
-        buffer = props.data || null
+    let start = g.in( 'start' ), end = g.in( 'end' ), 
+        rate = g.in( 'rate' ), shouldLoop = g.in( 'loops' ),
+        initialized  = g.param( 'initailied', 0 )
 
+    /* create dummy ugen until data for sampler is loaded...
+     * this will be overridden by a call to Gibberish.factory on load */
     syn.callback = function() { return 0 }
     syn.id = Gibberish.factory.getUID()
     syn.ugenName = syn.callback.ugenName = 'sampler_' + syn.id
     syn.inputNames = []
+    /* end dummy ugen */
+
+    syn.__bang__ = g.bang()
+    syn.trigger = syn.__bang__.trigger
 
     if( props.filename ) {
-      buffer = g.data( props.filename )
-      buffer.onload = () => {
-        Gibberish.factory( syn, peek( buffer, accum( 1, 0, { max: buffer.dim }), { mode:'samples' }), 'sampler', props ) 
+      syn.data = g.data( props.filename )
+
+      syn.data.onload = () => {
+        syn.__phase__ = g.counter( rate, start, end, syn.__bang__, shouldLoop, { shouldWrap:false })
+
+        Gibberish.factory( 
+          syn,
+          g.ifelse( 
+            g.and( g.gte( syn.__phase__, start ), g.lt( syn.__phase__, end ) ),
+            g.peek( 
+              syn.data, 
+              syn.__phase__,
+              { mode:'samples' }
+            ),
+            0
+          ),
+          'sampler', 
+          props 
+        ) 
+
+        if( syn.end === -99999 ) syn.end = syn.data.buffer.length - 1
+        
+        Gibberish.dirty( syn )
       }
-
-      console.log( 'sampler: loaded!', buffer.dim )
-      Gibberish.dirty( syn )
     }
-
-    //let sampleReader,
-    //    out = g.ifelse( initialized ),
-    //    panner
-
-    //if( props.panVoices === true ) { 
-    //  panner = g.pan( oscWithGain, oscWithGain, g.in( 'pan' ) ) 
-    //  Gibberish.factory( syn, [panner.left, panner.right], 'sampler', props  )
-    //}else{
-    //  Gibberish.factory( syn, oscWithGain , 'sampler', props )
-    //}
-    
-    //syn.env = env
 
     return syn
   }
@@ -51,10 +72,13 @@ module.exports = function( Gibberish ) {
     gain: 1,
     pan: .5,
     rate: 1,
-    panVoices:false
+    panVoices:false,
+    loops: 0,
+    start:0,
+    end:-99999
   }
 
-  let PolySampler = Gibberish.PolyTemplate( Sampler, ['rate','pan','gain'] ) 
+  let PolySampler = Gibberish.PolyTemplate( Sampler, ['rate','pan','gain','start','end','loops'] ) 
 
   return [ Sampler, PolySampler ]
 
