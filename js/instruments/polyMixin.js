7d03479a28ee66265f48aa82b146f433372ad351
charlieroberts <charlie@charlie-roberts.com>
2016-12-07 20:19:49 -0500

    adding polyphonic mixin

---
+++ js/instruments/polyMixin.js
@@ -0,0 +1,70 @@
+module.exports = {
+  note( freq ) {
+    let voice = this.__getVoice__()
+    Object.assign( voice, this.properties )
+    voice.note( freq )
+    this.__runVoice__( voice, this )
+    this.triggerNote = freq
+  },
+
+  // XXX this is not particularly satisfying...
+  // must check for both notes and chords
+  trigger( gain ) {
+    if( this.triggerChord !== null ) {
+      this.triggerChord.forEach( v => {
+        let voice = this.__getVoice__()
+        Object.assign( voice, this.properties )
+        voice.note( v )
+        voice.gain = gain
+        this.__runVoice__( voice, this )
+      })
+    }else if( this.triggerNote !== null ) {
+      let voice = this.__getVoice__()
+      Object.assign( voice, this.properties )
+      voice.note( this.triggerNote )
+      voice.gain = gain
+      this.__runVoice__( voice, this )
+    }else{
+      let voice = this.__getVoice__()
+      Object.assign( voice, this.properties )
+      voice.trigger( gain )
+      this.__runVoice__( voice, this )
+    }
+  },
+
+  __runVoice__( voice, _poly ) {
+    if( !voice.isConnected ) {
+      voice.connect( _poly, 1 )
+      voice.isConnected = true
+    }
+
+    let envCheck
+    if( _poly.envCheck === undefined ) {
+      envCheck = function() {
+        if( voice.env.isComplete() ) {
+          _poly.disconnectUgen.call( _poly, voice )
+          voice.isConnected = false
+        }else{
+          Gibberish.blockCallbacks.push( envCheck )
+        }
+      }
+    }else{
+      envCheck = poly.envCheck( voice, poly )
+    }
+
+    Gibberish.blockCallbacks.push( envCheck )
+  },
+
+  __getVoice__() {
+    return this.voices[ this.voiceCount++ % this.voices.length ]
+  },
+
+  chord( frequencies ) {
+    frequencies.forEach( v => this.note( v ) )
+    this.triggerChord = frequencies
+  },
+
+  free() {
+    for( let child of this.voices ) child.free()
+  }
+}
