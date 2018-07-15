42af801de235573f2d21642d0b346156a96b676d
charlieroberts <charlie@charlie-roberts.com>
2018-07-15 19:35:04 -0400

    added loudness features to snare ugen. updated package.json to include gulp-uglify-es

--- js/instruments/snare.js
+++ js/instruments/snare.js
@@ -9,7 +9,8 @@ module.exports = function( Gibberish ) {
         scaledDecay = g.mul( decay, g.gen.samplerate * 2 ),
         snappy= g.in( 'snappy' ),
         tune  = g.in( 'tune' ),
-        gain  = g.in( 'gain' )
+        gain  = g.in( 'gain' ),
+        loudness = g.in( 'loudness' )
 
     let props = Object.assign( {}, Snare.defaults, argumentProps )
 
@@ -17,11 +18,11 @@ module.exports = function( Gibberish ) {
         check = g.memo( g.gt( eg, .0005 ) ),
         rnd = g.mul( g.noise(), eg ),
         hpf = g.svf( rnd, g.add( 1000, g.mul( g.add( 1, tune), 1000 ) ), .5, 1, false ),
-        snap = g.gtp( g.mul( hpf, snappy ), 0 ), // rectify
+        snap = g.mul( g.gtp( g.mul( hpf, snappy ), 0 ), loudness ), // rectify
         bpf1 = g.svf( eg, g.mul( 180, g.add( tune, 1 ) ), .05, 2, false ),
         bpf2 = g.svf( eg, g.mul( 330, g.add( tune, 1 ) ), .05, 2, false ),
         out  = g.memo( g.add( snap, bpf1, g.mul( bpf2, .8 ) ) ), //XXX why is memo needed?
-        scaledOut = g.mul( out, gain )
+        scaledOut = g.mul( out, g.mul( gain, loudness ) )
     
     // XXX TODO : make this work with ifelse. the problem is that poke ugens put their
     // code at the bottom of the callback function, instead of at the end of the
@@ -36,11 +37,12 @@ module.exports = function( Gibberish ) {
   }
   
   Snare.defaults = {
-    gain: 1,
+    gain: .5,
     frequency:1000,
     tune:0,
     snappy: 1,
-    decay:.1
+    decay:.1,
+    loudness:1
   }
 
   return Snare
