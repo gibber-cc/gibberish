704a001af4057d5f4ccd2fde4bf639894e4d8d26
charlieroberts <charlie@charlie-roberts.com>
2019-04-03 10:26:57 -0400

    added clap and polyconga/polytom instruments

---
+++ js/instruments/clap.dsp.js
@@ -0,0 +1,66 @@
+const g = require( 'genish.js' ),
+      instrument = require( './instrument.js' )
+
+const genish = g
+  
+module.exports = function( Gibberish ) {
+
+  const Clap = argumentProps => {
+    'use jsdsp' 
+
+    const clap = Object.create( instrument ),
+          decay = g.in( 'decay' ), // 0-1 input value
+          scaledDecay = decay * (g.gen.samplerate * 2 ),
+          gain  = g.in( 'gain' ),
+          spacing = g.in( 'spacing' ), // spacing between clap, in Hzs
+          loudness = g.in( 'loudness' ),
+          cutoff = g.in('cutoff'),
+          Q      = g.in('Q')
+
+    const props = Object.assign( {}, Clap.defaults, argumentProps )
+
+    const eg = g.decay( scaledDecay, { initValue:0 } ), 
+          check = g.gt( eg, .0005 ),
+          noise = -1 + g.noise() * 2,
+          rnd = noise,//g.gtp( noise, 0 ),// * eg,
+          b   = g.bang(),
+          saw = g.phasor( spacing, b, { min:0 }),
+          rsaw = 1 - saw,
+          saw_env = g.ad( 0, .035 * g.gen.samplerate, { shape:'linear' }), 
+          b2 = g.bang(),
+          count = g.accum( 1,b2,{ max:Infinity, min:0, initialValue:0 }),
+          delayedNoise = g.switch( g.gte( count, g.gen.samplerate * .035 ), rnd, 0 ),
+          bpf1 = g.svf( delayedNoise, 1000, .5, 2, false ),
+
+          scaledOut = ( bpf1 * eg + ( rnd * rsaw * saw_env ) ) * gain * loudness,
+          out = g.svf( scaledOut, cutoff, Q, 1, false )
+    
+    // XXX TODO : make this work with ifelse. the problem is that poke ugens put their
+    // code at the bottom of the callback function, instead of at the end of the
+    // associated if/else block.
+    const ife = g.switch( check, out, 0 )
+    
+    clap.env = {
+      trigger( vol ) {
+        b.trigger()
+        eg.trigger( vol )
+        b2.trigger()
+        saw_env.trigger()
+      }
+    } 
+    
+    return Gibberish.factory( clap, ife, ['instruments','clap'], props  )
+  }
+  
+  Clap.defaults = {
+    gain: 1,
+    spacing:100,
+    decay:.2,
+    loudness:1,
+    cutoff:900,
+    Q:.85
+  }
+
+  return Clap
+
+}
--- js/instruments/instruments.js
+++ js/instruments/instruments.js
@@ -2,12 +2,12 @@ module.exports = function( Gibberish ) {
 
 const instruments = {
   Kick        : require( './kick.js' )( Gibberish ),
-  Conga       : require( './conga.js' )( Gibberish ),
-  Clave       : require( './conga.js' )( Gibberish ), // clave is same as conga with different defaults, see below
+  Clave       : require( './conga.js' )( Gibberish )[0], // clave is same as conga with different defaults, see below
   Hat         : require( './hat.js' )( Gibberish ),
   Snare       : require( './snare.js' )( Gibberish ),
   Cowbell     : require( './cowbell.js' )( Gibberish ),
-  Tom         : require( './tom.js' )( Gibberish )
+  Tom         : require( './tom.js' )( Gibberish ),
+  Clap        : require( './clap.js' )( Gibberish )
 }
 
 instruments.Clave.defaults.frequency = 2500
@@ -18,6 +18,7 @@ instruments.Clave.defaults.decay = .5;
 [ instruments.FM, instruments.PolyFM ]           = require( './fm.js' )( Gibberish );
 [ instruments.Sampler, instruments.PolySampler ] = require( './sampler.js' )( Gibberish );
 [ instruments.Karplus, instruments.PolyKarplus ] = require( './karplusstrong.js' )( Gibberish );
+[ instruments.Conga, instruments.PolyConga ]    = require( './conga.js' )( Gibberish )
 
 instruments.export = target => {
   for( let key in instruments ) {
