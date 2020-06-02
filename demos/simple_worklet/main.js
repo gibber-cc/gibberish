e9a3bf611a0454439bd4f31db6b4af7e3afce6b1
charlieroberts <charlie@charlie-roberts.com>
2020-06-02 16:43:12 -0700

    updated standalone example

--- demos/simple_worklet/main.js
+++ demos/simple_worklet/main.js
@@ -4,15 +4,24 @@ window.onload = function() {
 
   // Gibberish.init will wait to initialize audio context until
   // the browser window has received a mousedown or a touchdown
-  // event, which is required in Chrome >66.
+  // event, which is required in modern browsers. 
   Gibberish.init().then( ()=> {
-   let syn = Gibberish.instruments.Synth().connect()
+    Gibberish.export( window )
+    
+    fx = Chorus().connect( 
+      Freeverb().connect() 
+    )
 
-   let seq = Gibberish.Sequencer({
+    syn = Synth()
+      .connect( fx, 1 )
+      .connect()
+
+    seq = Sequencer({
       key:'note',
       target:syn,
       values:[ 220,440 ],
       timings:[ 22050 ]
     }).start()
+
   })
 } 
