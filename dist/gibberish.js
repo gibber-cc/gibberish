c657798e1f553e960abd0a6b8f266afe368539bf
charlieroberts <charlie@charlie-roberts.com>
2022-01-03 20:00:23 -0800

    build

--- dist/gibberish.js
+++ dist/gibberish.js
@@ -2590,8 +2590,6 @@ let proto = {
     gen.memo[ this.name ] = this.name
 
     return [this.name, out]
-    
-    return out
   }
 }
 
@@ -2628,8 +2626,6 @@ let proto = {
     gen.memo[ this.name ] = this.name
 
     return [this.name, out]
-    
-    return out
   }
 }
 
@@ -3549,8 +3545,6 @@ const proto = {
     gen.memo[ this.name ] = this.name
 
     return [this.name, out]
-    
-    return out
   }
 }
 
@@ -11965,10 +11959,12 @@ module.exports = function (Gibberish) {
           } else if (obj !== undefined) {
             const propSplit = propName.split('.');
             if (obj[propSplit[0]] !== undefined) {
-              if (typeof obj[propSplit[0]][propSplit[1]] !== 'function') {
-                obj[propSplit[0]][propSplit[1]] = value;
-              } else {
-                obj[propSplit[0]][propSplit[1]](value);
+              if (propSplit[1] !== undefined) {
+                if (typeof obj[propSplit[0]][propSplit[1]] !== 'function') {
+                  obj[propSplit[0]][propSplit[1]] = value;
+                } else {
+                  obj[propSplit[0]][propSplit[1]](value);
+                }
               }
             } else {
               //console.log( 'undefined split property!', id, propSplit[0], propSplit[1], value, obj )
@@ -13746,8 +13742,111 @@ function functionBindPolyfill(context) {
 })(this);
 
 },{}],159:[function(require,module,exports){
-arguments[4][81][0].apply(exports,arguments)
-},{"dup":81}],160:[function(require,module,exports){
+'use strict'
+
+let MemoryHelper = {
+  create( sizeOrBuffer=4096, memtype=Float32Array ) {
+    let helper = Object.create( this )
+
+    // conveniently, buffer constructors accept either a size or an array buffer to use...
+    // so, no matter which is passed to sizeOrBuffer it should work.
+    Object.assign( helper, {
+      heap: new memtype( sizeOrBuffer ),
+      list: {},
+      freeList: {}
+    })
+
+    return helper
+  },
+
+  alloc( size, immutable ) {
+    let idx = -1
+
+    if( size > this.heap.length ) {
+      throw Error( 'Allocation request is larger than heap size of ' + this.heap.length )
+    }
+
+    for( let key in this.freeList ) {
+      let candidate = this.freeList[ key ]
+
+      if( candidate.size >= size ) {
+        idx = key
+
+        this.list[ idx ] = { size, immutable, references:1 }
+
+        if( candidate.size !== size ) {
+          let newIndex = idx + size,
+              newFreeSize
+
+          for( let key in this.list ) {
+            if( key > newIndex ) {
+              newFreeSize = key - newIndex
+              this.freeList[ newIndex ] = newFreeSize
+            }
+          }
+        }
+
+        break
+      }
+    }
+
+    if( idx !== -1 ) delete this.freeList[ idx ]
+
+    if( idx === -1 ) {
+      let keys = Object.keys( this.list ),
+          lastIndex
+
+      if( keys.length ) { // if not first allocation...
+        lastIndex = parseInt( keys[ keys.length - 1 ] )
+
+        idx = lastIndex + this.list[ lastIndex ].size
+      }else{
+        idx = 0
+      }
+
+      this.list[ idx ] = { size, immutable, references:1 }
+    }
+
+    if( idx + size >= this.heap.length ) {
+      throw Error( 'No available blocks remain sufficient for allocation request.' )
+    }
+    return idx
+  },
+
+  addReference( index ) {
+    if( this.list[ index ] !== undefined ) { 
+      this.list[ index ].references++
+    }
+  },
+
+  free( index ) {
+    if( this.list[ index ] === undefined ) {
+      throw Error( 'Calling free() on non-existing block.' )
+    }
+
+    let slot = this.list[ index ]
+    if( slot === 0 ) return
+    slot.references--
+
+    if( slot.references === 0 && slot.immutable !== true ) {    
+      this.list[ index ] = 0
+
+      let freeBlockSize = 0
+      for( let key in this.list ) {
+        if( key > index ) {
+          freeBlockSize = key - index
+          break
+        }
+      }
+
+      this.freeList[ index ] = freeBlockSize
+    }
+  },
+}
+
+module.exports = MemoryHelper
+
+},{}],160:[function(require,module,exports){
 // shim for using process in browser
 var process = module.exports = {};
 
