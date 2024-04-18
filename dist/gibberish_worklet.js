e9fb661760e6008e5edd9e4392a9ad6659c9842b
charlieroberts <charlie@charlie-roberts.com>
2024-04-18 07:22:05 -0700

    soundfont chord changes

--- dist/gibberish_worklet.js
+++ dist/gibberish_worklet.js
@@ -15,6 +15,7 @@ const global = typeof window === 'undefined' ? {} : window;
             round = null,
             min   = null,
             max   = null,
+            line  = null,
             g     = null
 
         let initialized = false;
@@ -15466,9 +15467,16 @@ module.exports = function (Gibberish) {
       }
     },
 
-    chord(frequencies) {
+    chord(frequencies, loudnesses = null, durations = null) {
       if (Gibberish !== undefined && Gibberish.mode !== 'worklet') {
-        frequencies.forEach(v => this.note(v));
+        if (loudnesses === null && durations === null) {
+          frequencies.forEach(v => this.note(v));
+        } else if (durations === null) {
+          frequencies.forEach((v, i) => this.note(v, typeof loudnesses === 'object' ? loudnesses[i] : loudnesses));
+        } else {
+          frequencies.forEach((v, i) => this.note(v, typeof loudnesses === 'object' ? loudnesses[i] : loudnesses, typeof durations === 'object' ? durations[i] : durations));
+        }
+
         this.triggerChord = frequencies;
       }
     },
